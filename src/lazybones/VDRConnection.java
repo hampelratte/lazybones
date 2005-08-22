6920cc9bdfa1eb9dc756d4102cde6982285bb9fa
hampelratte <hampelratte>
2005-08-22 11:24:19 +0000

    connection now supports timeouts. still has to be integrated into settings/settingspanel

--- src/lazybones/VDRConnection.java
+++ src/lazybones/VDRConnection.java
@@ -8,46 +8,46 @@ import de.hampelratte.svdrp.Response;
 
 /**
  * @author <a href="hampelratte@users.sf.net>hampelratte@users.sf.net </a>
- *  
+ * 
  */
 public class VDRConnection {
-  private static final util.ui.Localizer mLocalizer = util.ui.Localizer
-      .getLocalizerFor(VDRConnection.class);
+    private static final util.ui.Localizer mLocalizer = util.ui.Localizer
+            .getLocalizerFor(VDRConnection.class);
 
-  private static Connection connection;
+    private static Connection connection;
 
-  protected static String host;
+    protected static String host;
 
-  protected static int port;
+    protected static int port;
+    
+    protected static int timeout = 500;
 
-
-  public static Response send(Command cmd) {
-    Response res = null;
-    try {
-      connection = new Connection(VDRConnection.host, VDRConnection.port);
-      Connection.DEBUG = false;
-      res = connection.send(cmd);
-      connection.close();
-    } catch (Exception e1) {
-      JOptionPane.showMessageDialog(null, mLocalizer.msg("couldnt_connect",
-          "Couldn't connect to VDR")+ ":\n" + e1.toString());
-    } 
-    return res;
-  }
-
-
-  public static boolean isAvailable() {
-    try {
-      connection = new Connection(VDRConnection.host, VDRConnection.port);
-      connection.close();
-      return true;
-    } catch (Exception e1) {
+    public static Response send(Command cmd) {
+        Response res = null;
+        try {
+            connection = new Connection(VDRConnection.host, VDRConnection.port, VDRConnection.timeout);
+            Connection.DEBUG = false;
+            res = connection.send(cmd);
+            connection.close();
+        } catch (Exception e1) {
+            JOptionPane.showMessageDialog(null, mLocalizer.msg(
+                    "couldnt_connect", "Couldn't connect to VDR")
+                    + ":\n" + e1.toString());
+        }
+        return res;
     }
-    return false;
-  }
 
+    public static boolean isAvailable() {
+        try {
+            connection = new Connection(VDRConnection.host, VDRConnection.port, VDRConnection.timeout);
+            connection.close();
+            return true;
+        } catch (Exception e1) {
+        }
+        return false;
+    }
 
-  public static Connection getConnection() {
-    return connection;
-  }
+    public static Connection getConnection() {
+        return connection;
+    }
 }
