cb5e41c0fcb221a8444948aa7e2e9131413783dc
hampelratte <hampelratte>
2006-04-01 16:02:10 +0000

    removed a try-catch-block

--- src/lazybones/ConnectionTester.java
+++ src/lazybones/ConnectionTester.java
@@ -1,16 +1,19 @@
 package lazybones;
 
+import de.hampelratte.svdrp.Connection;
 import de.hampelratte.svdrp.Response;
 import de.hampelratte.svdrp.commands.STAT;
 
 public class ConnectionTester extends Thread {
     
-    private boolean running = true;
+    private boolean running = false;
     private static ConnectionTester ct = null;
     
     private boolean connected = false;
     
-    private ConnectionTester() {}
+    private ConnectionTester() {
+        start();
+    }
     
     public static ConnectionTester getInstance() {
         if(ct == null) {
@@ -18,24 +21,39 @@ public class ConnectionTester extends Thread {
         }
         return ct;
     }
+
+    public void startThread() {
+        running = true;
+    }
     
     public void stopThread() {
         running = false;
     }
     
     public void run() {
-        while(running) {
-            try {
-                Response res = VDRConnection.send(new STAT());
-                if(res != null) {
-                    connected = true;
-                } else {
+        Response res = null;
+        while(true) {
+            if(running) {
+                try {
+                    Connection connection = new Connection(VDRConnection.host, VDRConnection.port,
+                            VDRConnection.timeout);
+                    Connection.DEBUG = false;
+                    res = connection.send(new STAT());
+                    connection.close();
+                    
+                    if(res != null) {
+                        connected = true;
+                        System.out.println("CONNECTED");
+                    } else {
+                        connected = false;
+                        System.out.println("DISCONNECTED");
+                    }
+                } catch (Exception e) {
                     connected = false;
+                    System.out.println("DISCONNECTED");
                 }
-                Thread.sleep(1000);
-            } catch (Exception e) {
-                connected = false;
             }
+            try { Thread.sleep(1000); } catch (InterruptedException e) { e.printStackTrace(); }
         }
     }
 
