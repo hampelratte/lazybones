21b5b185dd1e50283c2a1be5f45355c8220d146c
hampelratte <hampelratte>
2008-04-25 13:36:05 +0000

    added detail textarea to display the whole LogRecord

--- src/lazybones/gui/LogMessageDialog.java
+++ src/lazybones/gui/LogMessageDialog.java
@@ -1,4 +1,4 @@
-/* $Id: LogMessageDialog.java,v 1.12 2008-04-25 12:00:37 hampelratte Exp $
+/* $Id: LogMessageDialog.java,v 1.13 2008-04-25 13:36:05 hampelratte Exp $
  * 
  * Copyright (c) 2005, Henrik Niehaus & Lazy Bones development team
  * All rights reserved.
@@ -47,22 +47,43 @@ import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTextArea;
 import javax.swing.ListCellRenderer;
+import javax.swing.SwingConstants;
 import javax.swing.UIManager;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 import lazybones.LazyBones;
+import lazybones.logging.SimpleFormatter;
 import util.ui.Localizer;
 
 
 
-public class LogMessageDialog extends JDialog {
-    
-    // TODO add a threadsafe queue for saving the messages
+
+/**
+* This code was edited or generated using CloudGarden's Jigloo
+* SWT/Swing GUI Builder, which is free for non-commercial
+* use. If Jigloo is being used commercially (ie, by a corporation,
+* company or business for any purpose whatever) then you
+* should purchase a license for each developer using Jigloo.
+* Please visit www.cloudgarden.com for details.
+* Use of Jigloo implies acceptance of these licensing terms.
+* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED FOR
+* THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED
+* LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
+*/
+public class LogMessageDialog extends JDialog implements ListSelectionListener {
     
     private static LogMessageDialog instance;
     
     public JList list;
     public DefaultListModel model;
+    private JSplitPane splitPane;
+    private JScrollPane listScrollpane;
+    private JTextArea taDetails;
+    private JScrollPane textScrollpane;
     private HashMap<Level,Icon> icons = new HashMap<Level,Icon>();
 
     private LogMessageDialog() {
@@ -70,7 +91,6 @@ public class LogMessageDialog extends JDialog {
         initGUI();
     }
     
-
     private void initGUI() {
         setTitle("Lazy Bones - " + Localizer.getLocalization(Localizer.I18N_ERROR));
         setSize(700, 400);
@@ -83,13 +103,7 @@ public class LogMessageDialog extends JDialog {
         icons.put(Level.FINE,    UIManager.getDefaults().getIcon("OptionPane.informationIcon"));
         icons.put(Level.FINER,   UIManager.getDefaults().getIcon("OptionPane.informationIcon"));
         icons.put(Level.FINEST,  UIManager.getDefaults().getIcon("OptionPane.informationIcon"));
-        
-        
-        list = new JList();
-        model = new DefaultListModel();
-        list.setModel(model);
-        list.setCellRenderer(new LogListCellRenderer());
-        getContentPane().add(new JScrollPane(list), BorderLayout.CENTER);
+
         JButton okButton = new JButton(Localizer.getLocalization(Localizer.I18N_OK));
         okButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent event) {
@@ -97,7 +111,8 @@ public class LogMessageDialog extends JDialog {
             } 
         });
         getContentPane().add(okButton, BorderLayout.SOUTH);
-        
+        getContentPane().add(getSplitPane(), BorderLayout.CENTER);
+
         setDefaultCloseOperation(JDialog.HIDE_ON_CLOSE);
         addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent arg0) {
@@ -108,30 +123,67 @@ public class LogMessageDialog extends JDialog {
 
     
     private void closeWindow() {
-        model.removeAllElements();
         setVisible(false);
+        model.removeAllElements();
     }
 
     public synchronized static LogMessageDialog getInstance() {
         if (instance == null) {
             instance = new LogMessageDialog();
+            int parentWidth = LazyBones.getInstance().getParent().getWidth();
+            int parentHeight = LazyBones.getInstance().getParent().getHeight();
+            int posX = (parentWidth - instance.getWidth()) / 2;
+            int posY = (parentHeight - instance.getHeight()) / 2;
+            instance.setLocation(posX, posY);
         }
         return instance;
-    }
-    
-    public void setVisible(boolean visible) {
-        int parentWidth = LazyBones.getInstance().getParent().getWidth();
-        int parentHeight = LazyBones.getInstance().getParent().getHeight();
-        int posX = (parentWidth - getWidth()) / 2;
-        int posY = (parentHeight - getHeight()) / 2;
-        setLocation(posX, posY);
-        super.setVisible(visible);
     }
     
     public synchronized void addMessage(LogRecord message) {
         ((DefaultListModel)list.getModel()).addElement(message);
     }
     
+    private JSplitPane getSplitPane() {
+        if(splitPane == null) {
+            splitPane = new JSplitPane();
+            splitPane.add(getListScrollpane(), JSplitPane.LEFT);
+            splitPane.add(getTextScrollpane(), JSplitPane.RIGHT);
+            splitPane.setOrientation(SwingConstants.HORIZONTAL);
+            splitPane.setDividerLocation(150);
+        }
+        return splitPane;
+    }
+    
+    private JScrollPane getListScrollpane() {
+        if(listScrollpane == null) {
+            list = new JList();
+            list.addListSelectionListener(this);
+            model = new DefaultListModel();
+            list.setModel(model);
+            list.setCellRenderer(new LogListCellRenderer());
+            listScrollpane = new JScrollPane(list);
+        }
+        return listScrollpane;
+    }
+    
+    private JScrollPane getTextScrollpane() {
+        if(textScrollpane == null) {
+            textScrollpane = new JScrollPane();
+            textScrollpane.setViewportView(getTaDetails());
+        }
+        return textScrollpane;
+    }
+    
+    private JTextArea getTaDetails() {
+        if(taDetails == null) {
+            taDetails = new JTextArea();
+            taDetails.setText("Details...");
+            taDetails.setWrapStyleWord(true);
+            taDetails.setEditable(false);
+        }
+        return taDetails;
+    }
+
     private class LogListCellRenderer extends JLabel implements ListCellRenderer {
 
         private Color altColor = new Color(250, 250, 220);
@@ -143,19 +195,29 @@ public class LogMessageDialog extends JDialog {
         public Component getListCellRendererComponent(JList list, Object value, int index, boolean selected, boolean hasFocus) {
             LogRecord log = (LogRecord)value;
             StringBuilder sb = new StringBuilder("<html>");
-            sb.append(log.getSourceClassName());
-            sb.append('.');
-            sb.append(log.getSourceMethodName());
-            sb.append("<br>");
             sb.append(log.getMessage().replaceAll("\n", "<br>"));
             sb.append("</html>");
             
             setText(sb.toString());
             setIcon(icons.get(log.getLevel()));
             setForeground(Color.BLACK);
-            setBackground(index % 2 == 0 ? Color.WHITE : altColor);
+            
+            if(selected) {
+                setBackground(UIManager.getColor("List.selectionBackground"));
+            } else {
+                setBackground(index % 2 == 0 ? Color.WHITE : altColor);
+            }
             
             return this;
         }
+        
+        
+    }
+
+    private SimpleFormatter formatter = new SimpleFormatter();
+    public void valueChanged(ListSelectionEvent e) {
+        LogRecord log = (LogRecord) list.getSelectedValue();
+        taDetails.setText(formatter.format(log));
+        taDetails.setCaretPosition(0);
     }
 }
