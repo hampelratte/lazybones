e20e83f5ab1273aa7d99f0b909e6a720d3ac3333
hampelratte <hampelratte>
2008-10-21 19:42:35 +0000

    use criteria mechanism to assign timers to programs

--- src/lazybones/programmanager/ProgramManager.java
+++ src/lazybones/programmanager/ProgramManager.java
@@ -1,4 +1,4 @@
-/* $Id: ProgramManager.java,v 1.1 2008-10-17 21:24:56 hampelratte Exp $
+/* $Id: ProgramManager.java,v 1.2 2008-10-21 19:42:35 hampelratte Exp $
  * 
  * Copyright (c) 2005, Henrik Niehaus & Lazy Bones development team
  * All rights reserved.
@@ -35,7 +35,6 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Iterator;
 import java.util.List;
-import java.util.TreeMap;
 
 import javax.swing.JPopupMenu;
 
@@ -48,7 +47,6 @@ import lazybones.logging.LoggingConstants;
 import lazybones.logging.PopupHandler;
 import lazybones.programmanager.evaluation.Evaluator;
 import lazybones.programmanager.evaluation.Result;
-import lazybones.utils.Utilities;
 
 import org.hampelratte.svdrp.Response;
 import org.hampelratte.svdrp.commands.LSTE;
@@ -261,122 +259,84 @@ public class ProgramManager {
         // get the day program of the day, the previous day and the next day
         Date date = new Date(year, month, day);
         List<Program> threeDayProgram = getThreeDayProgram(date, chan); 
+        
         Iterator<Program> it = threeDayProgram.iterator(); 
-        if (it != null) {
-            // contains programs, which could be the right program for the timer
-            TreeMap<Integer, Program> candidates = new TreeMap<Integer, Program>();
-            
-            // contains programs, which start and stop between the start and the stop time
-            // of the timer and could be part of a Doppelpack
-            List<Program> doppelPack = new ArrayList<Program>();
-
-            // get timer start and end as unix time stamp
-            Calendar timerStartCal = bufferLessTimer.getStartTime();
-            Calendar timerEndCal = bufferLessTimer.getEndTime();
-            
-            // iterate over all programs and
-            // compare start and end time
-            while (it.hasNext()) { 
-                Program prog = it.next();
-                
-                // get prog start and end
-                Calendar progStartCal = prog.getDate().getCalendar();
-                progStartCal.set(Calendar.HOUR_OF_DAY, prog.getHours());
-                progStartCal.set(Calendar.MINUTE, prog.getMinutes());
-                progStartCal.set(Calendar.SECOND, 0);
-                Calendar progEndCal = (Calendar) progStartCal.clone();
-                progEndCal.add(Calendar.MINUTE, prog.getLength());
-                
-                // convert differences to minutes
-                int deltaStartMin = (int)Utilities.getDiffInMinutes(progStartCal, timerStartCal);
-                int deltaEndMin = (int)Utilities.getDiffInMinutes(progEndCal, timerEndCal);
-
-                // MAYBE zeittoleranz als option anbieten
-                // collect candidates, start and end time must not differ more than x minutes
-                int tolerance = 90;
-                if (deltaStartMin <= tolerance && deltaEndMin <= tolerance 
-                        || prog.getLength() == -1) /* special case prog.getLength() == -1, if the program is the last
-                                                     * program available in the EPG data. In this case, TVB can't calculate 
-                                                     * the length, because there is no subsequent program.*/
-                {
-                    candidates.put(new Integer(deltaStartMin), prog);
-                }
-                
-                // collect doppelpack candidates
-                // use timer with buffers
-                if(progStartCal.after(timer.getStartTime()) && progEndCal.before(timer.getEndTime())) {
-                    doppelPack.add(prog);
-                }
-            }
-
-            if (doppelPack.size() > 1) {
-                timer.setReason(Timer.NOT_FOUND); 
-                ArrayList<String> list = new ArrayList<String>();
-                String doppelpackTitle = null;
-                for (Program prog : doppelPack) {
-                    String title = prog.getTitle();
-                    if(list.contains(title)) {
-                        logger.debug("Doppelpack found: {}", title);
-                        timer.setReason(Timer.NO_REASON);
-                        doppelpackTitle = title;
-                    } else {
-                        list.add(title);
-                    }
-                }
-                
-                // mark all doppelpack programs
-                if(doppelpackTitle != null) {
-                    for (Program prog : doppelPack) {
-                        if(prog.getTitle().equals(doppelpackTitle)) {
-                            prog.mark(LazyBones.getInstance());
-                            timer.addTvBrowserProgID(prog.getID());
-                        }
-                    }
-                    return;
-                }
-                
-                // no doppelpack is found, this timer
-                // is a timer with weird start and end times,
-                // but we now add it to candidates, because, if
-                // it is not automatically assigned, we will do
-                // a lookup in stored timers as last resort
-                candidates.put(new Integer(0), doppelPack.get(0));
-            }
-            
-            // no candidate and no doppelpack found
-            // look for the timer in stored timers
-            if (candidates.size() == 0 && doppelPack.size() == 0) {
-                boolean found = TimerManager.getInstance().lookUpTimer(timer, null);
-                if (!found) {
-                    timer.setReason(Timer.NOT_FOUND);
-                }
-                
-                return;
-            }
-            
-            // we have some candidates and can now valuate the programs, with
-            // several criteria
-            Result bestMatching = evaluator.evaluate(candidates.values(), timer);
-            logger.info("Best matching program for timer {} is {} with a percentage of {}", new Object[] {timer.getTitle(), bestMatching.getProgram().getTitle(), bestMatching.getPercentage()});
-
-            int threshold = Integer.parseInt(LazyBones.getProperties().getProperty("percentageThreshold"));
-            // if the percentage of common words is higher than the config value
-            // percentageThreshold, mark this program
-            if (bestMatching.getPercentage() >= threshold) {
-                assignTimerToProgram(bestMatching.getProgram(), timer);
-            } else {
-                // no candidate and no doppelpack found
-                // look for the timer in stored timers
-                boolean found = TimerManager.getInstance().lookUpTimer(timer, bestMatching.getProgram());
-                if (!found) { // we have no mapping
-                    logger.warn("Couldn't find a program with that title: ", timer.getTitle());
-                    logger.warn("Couldn't assign timer: ", timer);
-                    timer.setReason(Timer.NOT_FOUND);
-                }
-            }
-        } else { // no channeldayprogram was found
+        if (it == null) {
             if(!timer.isRepeating()) {
                 timer.setReason(Timer.NO_EPG);
+            }
+            return;
+        }
+            
+        // contains programs, which start and stop between the start and the stop time
+        // of the timer and could be part of a Doppelpack
+        List<Program> doppelPack = new ArrayList<Program>();
+
+        // iterate over all programs and
+        // compare start and end time to collect doppelpack candidates
+        while (it.hasNext()) { 
+            Program prog = it.next();
+            
+            // get prog start and end
+            Calendar progStartCal = prog.getDate().getCalendar();
+            progStartCal.set(Calendar.HOUR_OF_DAY, prog.getHours());
+            progStartCal.set(Calendar.MINUTE, prog.getMinutes());
+            progStartCal.set(Calendar.SECOND, 0);
+            Calendar progEndCal = (Calendar) progStartCal.clone();
+            progEndCal.add(Calendar.MINUTE, prog.getLength());
+            
+            // collect doppelpack candidates
+            // use timer with buffers
+            if(progStartCal.after(timer.getStartTime()) && progEndCal.before(timer.getEndTime())) {
+                doppelPack.add(prog);
+            }
+        }
+
+        if (doppelPack.size() > 1) {
+            timer.setReason(Timer.NOT_FOUND); 
+            ArrayList<String> list = new ArrayList<String>();
+            String doppelpackTitle = null;
+            for (Program prog : doppelPack) {
+                String title = prog.getTitle();
+                if(list.contains(title)) {
+                    logger.debug("Doppelpack found: {}", title);
+                    timer.setReason(Timer.NO_REASON);
+                    doppelpackTitle = title;
+                } else {
+                    list.add(title);
+                }
+            }
+            
+            // mark all doppelpack programs
+            if(doppelpackTitle != null) {
+                for (Program prog : doppelPack) {
+                    if(prog.getTitle().equals(doppelpackTitle)) {
+                        prog.mark(LazyBones.getInstance());
+                        timer.addTvBrowserProgID(prog.getID());
+                    }
+                }
+                return;
+            }
+        }
+        
+        // no doppelpacks, we can now valuate the programs with
+        // several criteria
+        Result bestMatching = evaluator.evaluate(threeDayProgram, timer);
+        logger.debug("Best matching program for timer {} is {} with a percentage of {}", new Object[] {timer.getTitle(), bestMatching.getProgram().getTitle(), bestMatching.getPercentage()});
+
+        int threshold = Integer.parseInt(LazyBones.getProperties().getProperty("percentageThreshold"));
+        // if the percentage of equality is higher than the config value
+        // percentageThreshold, mark this program
+        if (bestMatching.getPercentage() >= threshold) {
+            assignTimerToProgram(bestMatching.getProgram(), timer);
+        } else {
+            // no candidate and no doppelpack found
+            // look for the timer in stored timers
+            boolean found = TimerManager.getInstance().lookUpTimer(timer, bestMatching.getProgram());
+            if (!found) { // we have no mapping
+                logger.warn("Couldn't find a program with that title: ", timer.getTitle());
+                logger.warn("Couldn't assign timer: ", timer);
+                timer.setReason(Timer.NOT_FOUND);
             }
         }
     }
