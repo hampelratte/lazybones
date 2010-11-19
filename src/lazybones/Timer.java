64d881b7832f4f39f4eecb8e4770a2a9087943c6
hampelratte <hampelratte>
2010-11-19 14:59:55 +0000

    * code formatting * fixed getDisplayTitle for timers with a path

--- src/lazybones/Timer.java
+++ src/lazybones/Timer.java
@@ -1,4 +1,4 @@
-/* $Id: Timer.java,v 1.17 2008-08-19 13:55:58 hampelratte Exp $
+/* $Id: Timer.java,v 1.18 2010-11-19 14:59:55 hampelratte Exp $
  * 
  * Copyright (c) 2005, Henrik Niehaus & Lazy Bones development team
  * All rights reserved.
@@ -39,30 +39,31 @@ import lazybones.utils.Period;
 import org.hampelratte.svdrp.responses.highlevel.VDRTimer;
 
 public class Timer extends VDRTimer {
-    
+
     public static final int NO_REASON = 0;
     public static final int NO_EPG = 1;
     public static final int NOT_FOUND = 2;
     public static final int NO_CHANNEL = 3;
     public static final int NO_PROGRAM = 4;
-    
+
     /**
      * The reason, why this Timer couldn't be assigned
      */
     private int reason = Timer.NO_REASON;
-    
+
     /**
      * List of all TV-B programs assigned to this timer
      */
     private List<String> tvBrowserProgIDs = new ArrayList<String>();
-    
+
     /**
      * Contains all time periods, where this timer conflicts with other timers
      */
     private List<Period> conflictPeriods = new ArrayList<Period>();
-    
-    public Timer() {}
-    
+
+    public Timer() {
+    }
+
     public Timer(VDRTimer timer) {
         super.setID(timer.getID());
         super.setState(timer.getState());
@@ -80,9 +81,10 @@ public class Timer extends VDRTimer {
         super.getStartTime().setTimeInMillis(timer.getStartTime().getTimeInMillis());
         super.setTitle(timer.getTitle());
     }
-    
+
     /**
      * Returns the IDs of all TV-Browser programs, which are assigned to this timer
+     * 
      * @return List of IDs of all TV-Browser programs, which are assigned to this timer
      */
     public List<String> getTvBrowserProgIDs() {
@@ -92,7 +94,7 @@ public class Timer extends VDRTimer {
     public void setTvBrowserProgIDs(List<String> tvBrowserProgIDs) {
         this.tvBrowserProgIDs = tvBrowserProgIDs;
     }
-    
+
     /**
      * @return Returns if this timer could be assigned to a Program
      */
@@ -101,7 +103,7 @@ public class Timer extends VDRTimer {
     }
 
     public Object clone() {
-        VDRTimer vdrtimer = (VDRTimer)super.clone();
+        VDRTimer vdrtimer = (VDRTimer) super.clone();
         Timer clone = new Timer(vdrtimer);
         clone.setTvBrowserProgIDs(getTvBrowserProgIDs());
         clone.setReason(getReason());
@@ -117,7 +119,9 @@ public class Timer extends VDRTimer {
 
     /**
      * Set the reason why this timer couldn't be assigned
-     * @param reason the reason why this timer couldn't be assigned
+     * 
+     * @param reason
+     *            the reason why this timer couldn't be assigned
      * @see #NO_CHANNEL
      * @see #NO_EPG
      * @see #NO_PROGRAM
@@ -135,17 +139,17 @@ public class Timer extends VDRTimer {
     public void addTvBrowserProgID(String id) {
         tvBrowserProgIDs.add(id);
     }
-    
+
     /**
-     * Returns, if <code>this</code> starts between the start time
-     * and the end time of the given timer <code>timer</code>.
+     * Returns, if <code>this</code> starts between the start time and the end time of the given timer
+     * <code>timer</code>.
+     * 
      * @param timer
      * @return
      */
     public boolean startsDuringTimer(Timer timer) {
-        if (this.getStartTime().compareTo(timer.getStartTime()) >= 0 && 
-            this.getStartTime().compareTo(timer.getEndTime()) <= 0) 
-        {
+        if (this.getStartTime().compareTo(timer.getStartTime()) >= 0
+                && this.getStartTime().compareTo(timer.getEndTime()) <= 0) {
             return true;
         }
         return false;
@@ -154,20 +158,23 @@ public class Timer extends VDRTimer {
     public List<Period> getConflictPeriods() {
         return conflictPeriods;
     }
-    
+
     public void addConflictPeriod(Period period) {
-        if(period.getStartTime().before(getStartTime())) {
+        if (period.getStartTime().before(getStartTime())) {
             period.setStartTime((Calendar) getStartTime().clone());
         }
-        if(period.getEndTime().after(getEndTime())) {
+        if (period.getEndTime().after(getEndTime())) {
             period.setEndTime((Calendar) getEndTime().clone());
         }
         getConflictPeriods().add(period);
     }
-    
+
     public String getDisplayTitle() {
-        return getPath() + getTitle();
-        
+        if (getPath() != null && getPath().length() > 0) {
+            return (getPath() + '/' + getTitle()).replace('~', '/');
+        } else {
+            return getTitle();
+        }
     }
 
     /**
@@ -175,7 +182,7 @@ public class Timer extends VDRTimer {
      * @return This timer without time buffers
      */
     public Timer getTimerWithoutBuffers() {
-        Timer timer = (Timer)this.clone();
+        Timer timer = (Timer) this.clone();
         int buffer_before = Integer.parseInt(LazyBones.getProperties().getProperty("timer.before"));
         timer.getStartTime().add(Calendar.MINUTE, buffer_before);
         int buffer_after = Integer.parseInt(LazyBones.getProperties().getProperty("timer.after"));
