3222610fa062a5cdf32de8c704f130bad2830821
Henrik Niehaus <henrik.niehaus@gmx.de>
2012-03-26 13:14:12 +0200

    added serialVersionId

--- src/lazybones/actions/responses/TimersOutOfSync.java
+++ src/lazybones/actions/responses/TimersOutOfSync.java
@@ -8,11 +8,11 @@
  * 
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice, 
- *    this list of conditions and the following disclaimer in the documentation 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 3. Neither the name of the project (Lazy Bones) nor the names of its 
- *    contributors may be used to endorse or promote products derived from this 
+ * 3. Neither the name of the project (Lazy Bones) nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
  *    software without specific prior written permission.
  * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
@@ -34,6 +34,8 @@ import lazybones.LazyBones;
 import org.hampelratte.svdrp.Response;
 
 public class TimersOutOfSync extends Response {
+
+    private static final long serialVersionUID = 1L;
 
     public TimersOutOfSync() {
         super(0, LazyBones.getTranslation("timers_out_of_sync", "Timers are out of sync"));
