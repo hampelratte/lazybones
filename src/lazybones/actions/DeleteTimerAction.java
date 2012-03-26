e6ef6435932b9ade8ec4fa23af2710f0dd6fc135
Henrik Niehaus <henrik.niehaus@gmx.de>
2012-03-26 13:26:54 +0200

    Added a small delay after deactivating a timer. Otherwise we still get the error, that this timer is recording. Seems to be a problem in VDR.

--- src/lazybones/actions/DeleteTimerAction.java
+++ src/lazybones/actions/DeleteTimerAction.java
@@ -8,11 +8,11 @@
  * 
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice, 
- *    this list of conditions and the following disclaimer in the documentation 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 3. Neither the name of the project (Lazy Bones) nor the names of its 
- *    contributors may be used to endorse or promote products derived from this 
+ * 3. Neither the name of the project (Lazy Bones) nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
  *    software without specific prior written permission.
  * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
@@ -109,6 +109,12 @@ public class DeleteTimerAction extends VDRAction {
             if (response == null || !(response instanceof R250)) {
                 return false;
             }
+        }
+
+        // wait a small amount of time. it seems, that this time is needed. otherwise we still get the error, that this timer is recording
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
         }
 
         // delete timer
