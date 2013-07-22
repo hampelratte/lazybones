042542e49701b487636030384b659ce9f6fc3dd8
Henrik Niehaus <henrik.niehaus@gmx.de>
2013-07-22 15:24:03 +0200

    Added copyright comment

--- src/lazybones/TitleMapping.java
+++ src/lazybones/TitleMapping.java
@@ -1,20 +1,19 @@
-/* $Id: TitleMapping.java,v 1.4 2011-04-20 12:09:11 hampelratte Exp $
- * 
+/*
  * Copyright (c) Henrik Niehaus & Lazy Bones development team
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice, 
- *    this list of conditions and the following disclaimer in the documentation 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 3. Neither the name of the project (Lazy Bones) nor the names of its 
- *    contributors may be used to endorse or promote products derived from this 
+ * 3. Neither the name of the project (Lazy Bones) nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
  *    software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -93,22 +92,27 @@ public class TitleMapping implements Serializable, TableModel {
 
     private ArrayList<TableModelListener> listeners = new ArrayList<TableModelListener>();
 
+    @Override
     public void addTableModelListener(TableModelListener l) {
         listeners.add(l);
     }
 
+    @Override
     public void removeTableModelListener(TableModelListener l) {
         listeners.remove(l);
     }
 
+    @Override
     public Class<?> getColumnClass(int columnIndex) {
         return String.class;
     }
 
+    @Override
     public int getColumnCount() {
         return 2;
     }
 
+    @Override
     public String getColumnName(int columnIndex) {
         if (columnIndex == 0) {
             return "TV-Browser";
@@ -117,10 +121,12 @@ public class TitleMapping implements Serializable, TableModel {
         }
     }
 
+    @Override
     public int getRowCount() {
         return vdrTitles.size();
     }
 
+    @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
         if (columnIndex == 0) {
             return tvbTitles.get(rowIndex);
@@ -129,10 +135,12 @@ public class TitleMapping implements Serializable, TableModel {
         }
     }
 
+    @Override
     public boolean isCellEditable(int rowIndex, int columnIndex) {
         return true;
     }
 
+    @Override
     public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
         if (columnIndex == 0) {
             tvbTitles.set(rowIndex, aValue.toString());
@@ -152,7 +160,7 @@ public class TitleMapping implements Serializable, TableModel {
     public void setMappingFromMap(Map<String, String> map) {
         for (Iterator<String> iter = map.keySet().iterator(); iter.hasNext();) {
             String key = iter.next();
-            String value = (String) map.get(key);
+            String value = map.get(key);
             tvbTitles.add(key);
             vdrTitles.add(value);
         }
