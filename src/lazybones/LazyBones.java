6f88eaeaf3554da027452add4f23688b43712b67
Henrik Niehaus <henrik.niehaus@gmx.de>
2016-04-09 13:43:45 +0200

    Set version to 1.5.2

--- src/lazybones/LazyBones.java
+++ src/lazybones/LazyBones.java
@@ -203,7 +203,7 @@ public class LazyBones extends Plugin implements Observer {
 
     public static Version getVersion() {
         // return new Version(0, 0, false, "snapshot-01-05-2013");
-        return new Version(1, 51, 0, true);
+        return new Version(1, 52, 0, true);
     }
 
     public MainDialog getMainDialog() {
